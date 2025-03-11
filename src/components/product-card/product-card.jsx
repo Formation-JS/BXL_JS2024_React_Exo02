55cb645c8e497b2add3d096d212427cd2789c2d0
Aurélien St <aurelien.strimelle@bstorm.be>
2025-03-11 14:29:25 +0100

    OnSale image 🎯

--- src/components/product-card/product-card.jsx
+++ src/components/product-card/product-card.jsx
@@ -1,6 +1,7 @@
 import clsx from 'clsx';
 import style from './product-card.module.css';
 import Price from '../price/price.jsx';
+import saleImage from './sale.png';
 
 export default function ProductCard({
     id, name, ref, desc, category, price, onSale
@@ -9,6 +10,9 @@ export default function ProductCard({
     return (
         <div className={style['product-card']}>
             <span>{name} [{category}]</span>
+            {onSale && (
+                <img src={saleImage} className={style['sale-image']} alt="En promo !" />
+            )}
             <span>Référence : {ref}</span>
             <span>{desc || 'Aucune description !'}</span>
             {/* <span className={style.price + ' ' + (onSale ? style.promo : '')}>{price}</span> */}
